2957810c496585543ec511cc137f5927bca29696
kaisertuan <thekaiser.tuan@gmail.com>
2022-12-19 22:12:49 +0700

    static file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
 import configViewEngine from './configs/viewEngine.js';
+dotenv.config ();
+
 const app = express ();
 
 configViewEngine (app);
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get ('/', (req, res) => {
   res.render ('index.ejs');
